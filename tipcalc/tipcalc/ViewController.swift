cfa803d202e05d2f119eb528c74764678ef97d70
Ana Cuculescu <amcuculescu@gmail.com>
2020-09-15 20:39:07 -0700

    almost there

--- tipcalc/tipcalc/ViewController.swift
+++ tipcalc/tipcalc/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        //get bill
+        
+        //calc tip
+        
+        //update labels
+        tipLabel.text = "100"
+        totalLabel.text = "10"
+    }
 }
 
