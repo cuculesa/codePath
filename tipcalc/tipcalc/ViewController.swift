538b9f169d39a48f082f8a8c8c958bc25f3667f9
Ana Cuculescu <amcuculescu@gmail.com>
2020-09-15 20:17:33 -0700

    init

---
+++ tipcalc/tipcalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalc
+//
+//  Created by Ana Cuculescu on 9/15/20.
+//  Copyright © 2020 Ana Cuculescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
